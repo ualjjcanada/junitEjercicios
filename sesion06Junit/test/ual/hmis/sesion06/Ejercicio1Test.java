e8ec6672e124bada59d24708d1cb4d0874e690a5
José Joaquín Cañadas Martínez <jjcanada@ual.es>
2020-03-30 03:19:58 +0200

    sesio06 folder

---
+++ sesion06Junit/test/ual/hmis/sesion06/Ejercicio1Test.java
@@ -0,0 +1,26 @@
+package ual.hmis.sesion06;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import ual.hmis.sesion06.Ejercicio1;
+
+
+class Ejercicio1Test {
+
+	@ParameterizedTest (name = "{index} => Con entrada ({0}) sale {1}")
+	@CsvSource({"2, 1",
+//				"5, 1",
+//				"6, 1",
+				"14, 7"})
+	
+	void testTransformar(int input, int expected) {
+		Ejercicio1 c = new Ejercicio1();
+		assertEquals(expected, c.transformar(input));
+	}
+
+}
